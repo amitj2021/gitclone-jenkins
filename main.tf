289c9de6fd25de3fcda07f6a51290f5dc6c874a1
amitj2021 <137259321+amitj2021@users.noreply.github.com>
2023-11-27 21:43:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,13 @@
 provider "azurerm" {
-  features {
-  
-  }
-}
+    client_id       = var.ARM_CLIENT_ID      
+    client_secret   = var.ARM_CLIENT_SECRET  
+    subscription_id = var.ARM_SUBSCRIPTION_ID
+    tenant_id       = var.ARM_TENANT_ID   
+    features {
 
+    }   
+  
+}
 resource "azurerm_resource_group" "ajrg-01" {
   name     = "rg-101"
   location = "East US"
